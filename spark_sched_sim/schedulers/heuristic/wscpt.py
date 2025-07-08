1) sjf local scheduling DRA fix 2) cpt fix (task duration -> stage duration) 3) node scales added for normalization

spark_sched_sim/schedulers/heuristic/wscpt.py
@@ -1,5 +1,7 @@
 import numpy as np
 from .heuristic import HeuristicScheduler
+from .heuristic_utils import parse_jobs_from_obs
+from spark_sched_sim.wrappers import DAGNNObsWrapper
 
 class WscptScheduler(HeuristicScheduler):
     def __init__(self, num_executors, resource_allocation):
@@ -7,76 +9,66 @@ class WscptScheduler(HeuristicScheduler):
         super().__init__(name)
         self.num_executors = num_executors
         self.resource_allocation = resource_allocation
-
+        self.obs_wrapper_cls = DAGNNObsWrapper
+        self.heuristic_idx = 0
     def schedule(self, obs):
-        #print("In WSCPT")
-        job_ptr = np.array(obs["dag_ptr"])
+        jobs = parse_jobs_from_obs(obs)
+        num_active_jobs = len(jobs)
         stage_mask = obs["stage_mask"]
-        stage_cpt = obs["dag_batch"].nodes[:,5]
-        masked_stages_cpt = np.multiply(stage_cpt,stage_mask)
+
         schedulable_stages = dict(zip(stage_mask.nonzero()[0], np.arange(stage_mask.sum())))
         exec_supplies = np.array(obs["exec_supplies"])
         num_committable_execs = obs["num_committable_execs"]
         source_job_idx = obs["source_job_idx"]
 
-        num_active_jobs = len(exec_supplies)
-        selected_job_idx = -1
-        num_exec = -1
-        if schedulable_stages:
-            # first, try to find a stage in the same job that is releasing executers
-            if source_job_idx < num_active_jobs:
-                stage_idx_start = job_ptr[source_job_idx]
-                stage_idx_end = job_ptr[source_job_idx + 1]
-                if stage_mask[stage_idx_start:stage_idx_end].sum() > 0:
-                    selected_job_idx = source_job_idx
-                    # print("DRA exec cap: ", obs["DRA_exec_cap"][selected_job_idx], "exec supplies: ",
-                    #       obs["exec_supplies"][selected_job_idx], "num committable execs: ",
-                    #       obs["num_committable_execs"])
-                    if self.resource_allocation == 'DRA':
-                        num_exec = min(obs["DRA_exec_cap"][selected_job_idx],num_committable_execs)-1
-                    else:
-                        num_exec = num_committable_execs-1
+        DRA_exec_cap = obs["DRA_exec_cap"]
+        dag_ptr = obs["dag_ptr"]
 
-            if selected_job_idx == -1:
-                # find job cpt
-                job_cpt = {}
-                for job_idx in range(num_active_jobs):
-                    #check if the job has any schedulable stages
-                    job_stages_cpt = masked_stages_cpt[np.arange(job_ptr[job_idx], job_ptr[job_idx + 1])]
-                    if len(job_stages_cpt.nonzero()[0]) > 0 :
-                        job_cpt[job_idx] = max(job_stages_cpt)
-
-                if self.resource_allocation == 'DRA':
-                    for job in range(num_active_jobs):
-                        selected_job_idx = min(job_cpt, key=job_cpt.get)
-                        # if obs["DRA_exec_cap"][selected_job_idx] < self.num_executors:
-                        #     # increase DRA exec_cap manually for the next job if the job that is releasing executors have the last stage to be processed.
-                        #     if job_ptr[1] == 1 and stage_mask[0] == False:
-                        #         obs["DRA_exec_cap"][selected_job_idx] = self.num_executors
-
-                        if obs["exec_supplies"][selected_job_idx] >= obs["DRA_exec_cap"][selected_job_idx]:
-                            job_cpt[selected_job_idx] = np.inf
-                            continue
-                    #If
-                    if job_cpt[min(job_cpt)] == np.inf:
-                        selected_job_idx = min(job_cpt, key=job_cpt.get)
-                        obs["DRA_exec_cap"][selected_job_idx] = self.num_executors
-
-                    num_exec = min(obs["DRA_exec_cap"][selected_job_idx] - obs["exec_supplies"][selected_job_idx],
-                                   obs["num_committable_execs"]) - 1
+        def compute_num_exec(job_idx):
+            if self.resource_allocation == 'DRA':
+                dra_cap = DRA_exec_cap.get(job_idx, 0)
+                if job_idx < num_active_jobs:
+                    return max(min(dra_cap, num_committable_execs), 0)
                 else:
-                    selected_job_idx = min(job_cpt, key=job_cpt.get)
+                    return max(min(dra_cap - exec_supplies[job_idx], num_committable_execs), 0)
+            else:
+                return num_committable_execs
 
-            """searches for a schedulable stage in a given job, prioritizing a node with the longest cpt"""
-            stage_idx_start = job_ptr[selected_job_idx]
-            stage_idx_end = job_ptr[selected_job_idx + 1]
+        def get_stage_idx(job_idx, stage):
+            global_idx = dag_ptr[job_idx] + stage.id_
+            return schedulable_stages[global_idx]
 
-            assert max(masked_stages_cpt[stage_idx_start:stage_idx_end]) > 0
-            node_selected = np.argmax(masked_stages_cpt[stage_idx_start:stage_idx_end]) + stage_idx_start
-            selected_stage_idx = schedulable_stages[node_selected]
+        # 1. Local Scheduling: within same job
+        if source_job_idx < num_active_jobs:
+            local_job = jobs[source_job_idx]
+            if local_job.frontier_stages:
+                num_exec = compute_num_exec(source_job_idx)
+                if num_exec > 0:
+                    selected_stage = max(local_job.frontier_stages, key=lambda s: s.cpt)
+                    #selected_stage = min(local_job.frontier_stages, key=lambda s: s.remaining_work)
+                    return {
+                        "heuristic_idx": self.heuristic_idx,
+                        "stage_idx": get_stage_idx(source_job_idx, selected_stage),
+                        "num_exec": num_exec - 1,
+                    }
 
-            return {"stage_idx": selected_stage_idx, "num_exec": num_exec}
+        # 2. Global SJF: Across all jobs
+        candidate_jobs = [
+            job for job in jobs if job.frontier_stages and compute_num_exec(job.id_) > 0
+        ]
 
-        else:
-            # didn't find any stages to schedule
-            return {"stage_idx": -1, "num_exec": num_committable_execs}
+        if not candidate_jobs:
+            print("No candidate jobs found for WSCPT scheduling.")
+            return {"heuristic_idx" : self.heuristic_idx, "stage_idx": -1, "num_exec": num_committable_execs - 1}
+
+        selected_job = min(candidate_jobs, key=lambda job: job.cpt)
+        selected_stage = max(selected_job.frontier_stages, key=lambda s: s.cpt)
+        #selected_job = min(candidate_jobs, key=lambda job: job.total_remaining_work)
+        #selected_stage = min(selected_job.frontier_stages, key=lambda s: s.remaining_work)
+
+        num_exec = compute_num_exec(selected_job.id_)
+        return {
+            "heuristic_idx" : self.heuristic_idx,
+            "stage_idx": get_stage_idx(selected_job.id_, selected_stage),
+            "num_exec": num_exec - 1,
+        }

trainers/ppo.py
@@ -53,7 +53,7 @@ class PPO(Trainer):
             returns_list,
             baselines_list,
             lgprobs_list,
-        ) = self._preprocess_rollouts(rollout_buffers) #trainer.py line169 -> Calculrate returns at k using rewards
+        ) = self._preprocess_rollouts(rollout_buffers) #trainer.py line169 -> Calculate returns at k using rewards
 
         returns = np.array(list(chain(*returns_list)))
         baselines = np.concatenate(baselines_list)